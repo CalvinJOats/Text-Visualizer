9223d7c1ae1430c96fd6109a970796e5314c1334
CalvinO <88681685+CalvinJOats@users.noreply.github.com>
2023-11-07 19:58:47 -0700

    Adding Existing Project Files
    An upload of the existing project files. The cmake will need to be updated and the Magick++ library will need to be imported.

---
+++ text_interpreter.cpp
@@ -0,0 +1,11 @@
+//
+// Created by calvin on 10/26/23.
+//
+
+#include "text_interpreter.h"
+#include <string>
+#include <iostream>
+
+std::string TextInterpreter::TextInput() {
+    return "";
+}
---
+++ text_interpreter.h
@@ -0,0 +1,19 @@
+//
+// Created by calvin on 10/26/23.
+//
+
+#ifndef TEXT_VISUALIZER_TEXT_INTERPRETER_H
+#define TEXT_VISUALIZER_TEXT_INTERPRETER_H
+
+#include <string>
+
+class TextInterpreter {
+private:
+
+
+public:
+    std::string TextInput();
+
+};
+
+#endif //TEXT_VISUALIZER_TEXT_INTERPRETER_H
